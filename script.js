6713dacf92b952bcc40e896539cae88da4bafd1c
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-17 00:49:13 +0000

    validation in arabic and english

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // A verse from the quran in the format surah:ayah (uthmanic codex order)
 // The url for the array with the verse specified by the user
+error = "undefined"
+
 async function updateVerse() {
+  arUpdateVerse()
   // get the verse the user has input
   let surahAyah = document.getElementById("verse").value
   // update the url using the verse they input
@@ -13,8 +16,12 @@ async function updateVerse() {
   let theVerse = surahAyah + ": " + verseText;
 
   // put the resulting verse on the website
-  document.getElementById("result").innerHTML = theVerse;
-  arUpdateVerse()
+  if (typeof verseText != "undefined") {
+    document.getElementById("result").innerHTML = theVerse;
+  }
+  if (typeof verseText == "undefined") {
+    document.getElementById("result").innerHTML = "This verse does not exist!";
+  }
 }
 
 async function arUpdateVerse() {
@@ -28,5 +35,10 @@ async function arUpdateVerse() {
   let arVerseText = arData.data.text
   let arTheVerse = arSurahAyah + ": " + arVerseText;
   // put the resulting verse on the website
-  document.getElementById("arResult").innerHTML = arTheVerse;
+  if (typeof arVerseText != "undefined") {
+    document.getElementById("arResult").innerHTML = arTheVerse;
+  }
+  if (typeof arVerseText == "undefined") {
+    document.getElementById("arResult").innerHTML = "هذه الآية غير موجودة!";
+  }
 }
